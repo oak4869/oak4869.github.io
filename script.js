0bc1039a012c103afbc40f5f08fe264920ab2807
oak4869 <59789086+oak4869@users.noreply.github.com>
2026-01-30 23:36:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,92 @@
-console.log("script.js loaded");
-
 const URL = "./model/";
 
-let model, webcam;
+let model, webcam, maxPredictions;
+let faceDetection;
+let currentLabel = "";
 
 async function init() {
-  console.log("init started");
-
   model = await tmImage.load(
     URL + "model.json",
     URL + "metadata.json"
   );
 
-  console.log("model loaded");
-  
-webcam = new tmImage.Webcam(300, 300, true);
-await webcam.setup();
-await webcam.play();
+  webcam = new tmImage.Webcam(300, 300, true);
+  await webcam.setup();
+  await webcam.play();
 
-console.log("canvas =", webcam.canvas);
+  document.getElementById("camera-container").appendChild(webcam.canvas);
 
-document.getElementById("camera-container").appendChild(webcam.canvas);
+  const overlay = document.getElementById("overlay");
+  overlay.width = 300;
+  overlay.height = 300;
 
- 
+  faceDetection = new FaceDetection({
+    locateFile: file =>
+      `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection/${file}`
+  });
+
+  faceDetection.setOptions({
+    model: "short",
+    minDetectionConfidence: 0.5
+  });
+
+  faceDetection.onResults(onFaceResults);
+
+  window.requestAnimationFrame(loop);
 }
 
-init();
 
+async function loop() {
+  webcam.update();
+  await predict();
+  await faceDetection.send({ image: webcam.canvas });
+  window.requestAnimationFrame(loop);
+}
+
+
+
+async function predict() {
+  const predictions = await model.predict(webcam.canvas);
+  predictions.sort((a, b) => b.probability - a.probability);
+
+  const best = predictions[0];
+  currentLabel = best.className;
+
+  const percent = (best.probability * 100).toFixed(2);
+
+  document.getElementById("result").innerHTML =
+    `ผลการตรวจจับ: <b>${best.className}</b><br>
+     ความมั่นใจ: <b>${percent}%</b>`;
+}
+
+
+function onFaceResults(results) {
+  const canvas = document.getElementById("overlay");
+  const ctx = canvas.getContext("2d");
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (results.detections.length > 0) {
+    results.detections.forEach(detection => {
+      const box = detection.boundingBox;
+
+      let color = "red";
+        if (currentLabel === "Mask") {
+            color = "green";
+        }
+
+
+      ctx.strokeStyle = color;
+      ctx.lineWidth = 4;
+      ctx.strokeRect(
+        box.xCenter * canvas.width - (box.width * canvas.width) / 2,
+        box.yCenter * canvas.height - (box.height * canvas.height) / 2,
+        box.width * canvas.width,
+        box.height * canvas.height
+      );
+    });
+  }
+}
+
+
+init();
