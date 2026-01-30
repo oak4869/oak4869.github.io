46d4f13373bffaaad08605f243bcdcea7bd27989
oak4869 <59789086+oak4869@users.noreply.github.com>
2026-01-30 23:25:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+console.log("script.js loaded");
+
 const URL = "./model/";
 
 let model, webcam;
-let currentLabel = "";
 
 async function init() {
   console.log("init started");
@@ -11,31 +12,13 @@ async function init() {
     URL + "metadata.json"
   );
 
+  console.log("model loaded");
+
   webcam = new tmImage.Webcam(300, 300, true);
   await webcam.setup();
   await webcam.play();
 
   document.getElementById("camera-container").appendChild(webcam.canvas);
-
-  window.requestAnimationFrame(loop);
-}
-
-async function loop() {
-  webcam.update();
-  await predict();
-  window.requestAnimationFrame(loop);
-}
-
-async function predict() {
-  const predictions = await model.predict(webcam.canvas);
-  predictions.sort((a, b) => b.probability - a.probability);
-
-  const best = predictions[0];
-  currentLabel = best.className;
-
-  document.getElementById("result").innerHTML =
-    `ผลการตรวจจับ: <b>${best.className}</b><br>
-     ความมั่นใจ: <b>${(best.probability * 100).toFixed(2)}%</b>`;
 }
 
 init();
